a9c4a2c3118b747670977ce48275e3f815d1c712
Justin Phillips <justinphil02@yahoo.com>
2020-01-19 01:29:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-    get '/' do 
+    get '/' do
         erb :index
-    end 
-    get '/new' do 
+    end
+    get '/new' do
         erb :create_puppy
-        
-    end 
+
+    end
     post '/puppy' do
         @puppy = Puppy.new(params['name'], params['breed'], params['age'])
         erb :display_puppy
